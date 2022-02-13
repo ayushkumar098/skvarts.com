09c80167c605894e34b3ac44a46e8cefc1b647a6
akshay <a.akshay7799@gmail.com>
2022-02-14 00:40:02 +0530

    port change

--- app.js
+++ app.js
@@ -209,6 +209,6 @@ app.get("/error",function(req,res){
 // })
 
 
-app.listen(3000, function(){
-    console.log("Server started on port 3000");
+app.listen(process.env.PORT, function(){
+    console.log("Server started on port " + process.env.PORT);
 })
